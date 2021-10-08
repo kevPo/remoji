07df15020fc92df6873924b0204d79d215a24f9c
Kevin Poston <kevposton@gmail.com>
2021-10-07 20:05:19 -0400

    trying something else

--- netlify/functions/remoji-add.ts
+++ netlify/functions/remoji-add.ts
@@ -7,16 +7,41 @@ const axios = require("axios");
 const FormData = require("form-data");
 const fetch = require("node-fetch");
 const fs = require("fs");
+import { RemoveBgResult, RemoveBgError, removeBackgroundFromImageUrl } from "remove.bg";
+ 
+const url = "https://www.remove.bg/example.jpg";
 const { REMOVE_BG_KEY } = process.env;
 
 const handler = async (event) => {
   console.log(`get it this time???`);
 
-  const subject = event.queryStringParameters.name || "World";
+  removeBackgroundFromImageUrl({
+    url,
+    apiKey: REMOVE_BG_KEY,
+    size: "regular",
+    type: "person"
+  }).then((result: RemoveBgResult) => {
+    const base64img = result.base64img;
+    console.log('got here');
+    console.log(base64img);
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "image/png",
+      },
+      body: base64img,
+      isBase64Encoded: true,
+    };
+    
+  }).catch((errors: Array<RemoveBgError>) => {
+   console.log(JSON.stringify(errors));
+  });
 
-  const formData = new FormData();
-  formData.append("size", "auto");
-  formData.append("image_url", "https://www.remove.bg/example.jpg");
+  // const subject = event.queryStringParameters.name || "World";
+
+  // const formData = new FormData();
+  // formData.append("size", "auto");
+  // formData.append("image_url", "https://www.remove.bg/example.jpg");
 
   // axios({
   //   method: "post",
@@ -48,30 +73,23 @@ const handler = async (event) => {
   //   body: `Hello ${subject}!`,
   // };
 
-  try {
-    const response = await fetch("https://api.remove.bg/v1.0/removebg", {
-      method: "POST",
-      headers: {
-        "X-Api-Key": REMOVE_BG_KEY,
-      },
-      body: formData,
-    });
+  // try {
+  //   const response = await fetch("https://api.remove.bg/v1.0/removebg", {
+  //     method: "POST",
+  //     headers: {
+  //       "X-Api-Key": REMOVE_BG_KEY,
+  //     },
+  //     body: formData,
+  //   });
 
-    let keys = Object.keys(response);
-    keys.forEach((k) => console.log(k));
+  //   let keys = Object.keys(response);
+  //   keys.forEach((k) => console.log(k));
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "image/png",
-      },
-      body: response,
-      isBase64Encoded: true,
-    };
-  } catch (error) {
-    console.log("uh oh");
-    return { statusCode: 422, body: String(error) };
-  }
+    
+  // } catch (error) {
+  //   console.log("uh oh");
+  //   return { statusCode: 422, body: String(error) };
+  // }
 };
 
 module.exports = { handler };
